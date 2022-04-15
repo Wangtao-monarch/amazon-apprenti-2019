cec2a91563650ebdb047d6f21d88e3341fbc7f52
Chen Haoran <56293353+CHR951753@users.noreply.github.com>
2022-04-15 21:04:05 +0800

    Update python.py

--- CS120/WangTao/WangTao2/python.py
+++ CS120/WangTao/WangTao2/python.py
@@ -1,4 +1,16 @@
-a=int(input())
-sum=0
-sum=3*(2**(a-1))-3+1
-print(sum)
+//Call a third-party library.1917494051Wang Tao-1917494045Chen Haoran-1917494031Zhang Junwei
+import pygame
+import sys
+import time
+import random
+from pygame.locals import *
+
+//Initial settings
+pygame.init()  //Initialize pygame
+fpsClock = pygame.display.set_mode((640,480))  //Create a pyGame display layer
+playSurface = pygame.display.set_mode((640,480))  
+pygame.display.set_caption('Snake Co!')  //Define title
+image = pygame.image.load('game.ico')  //Load resource picture, game ICO is included in the final file.
+pygame.display.set_icon(image) //Settings Icon
+
+//Define color variables
